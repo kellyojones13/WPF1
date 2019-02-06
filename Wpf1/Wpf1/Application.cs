20745934ae641db77229bab8caa69414494c32fc
Kelly Jones <kellyojones@ou.edu>
2019-02-06 11:03:55 -0600

    Participation1

---
+++ Wpf1/Wpf1/Application.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wpf1
+{
+    class Application
+    {
+        public string Name { get; set; }
+
+        public string Address { get; set; }
+
+        public int ZipCode { get; set; }
+
+    }
+
+    public Application (string name, string address, int zipCode); 
+}
